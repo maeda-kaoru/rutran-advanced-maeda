9ffaf2c55123436cf3ea5e7573e1b3d07904d7c5
yutamotegi <moyuta0408@gmail.com>
2023-04-24 19:16:59 +0900

    fix: ディレクトリの修正

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/motegiyuuta/Desktop/students/rutran-advanced-maeda/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default /* #__PURE__ */ defuFn(inlineConfig)
