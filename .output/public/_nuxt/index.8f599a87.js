522305532c7edd1a094efa6fc880395de95c8240
yutamotegi <moyuta0408@gmail.com>
2023-04-27 19:20:16 +0900

    fix: 不必要なファイルを削除

--- .output/public/_nuxt/index.8f599a87.js
+++
@@ -1,1 +0,0 @@
-import{a as e,b as o,o as t,e as c}from"./entry.c5415d84.js";const n={},s=c("p",null,"ページ",-1),a=[s];function r(_,d){return t(),o("div",null,a)}const i=e(n,[["render",r]]);export{i as default};
